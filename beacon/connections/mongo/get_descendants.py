97b5ae4aeb53208a74270049b21e6307ec7af4f0
magdasmat <magda.smat@gmail.com>
2026-07-28 14:42:40 +0200

    Fix descendant lookup in MongoDB

--- beacon/connections/mongo/get_descendants.py
+++ beacon/connections/mongo/get_descendants.py
@@ -1,10 +1,21 @@
 import obonet
 import networkx
 import os
+import gc
 import urllib.request
 from urllib.error import HTTPError
 import progressbar
 from beacon.connections.mongo.client import get_client
+
+# to support different paths for admin-ui and beaconprod
+ONTOLOGIES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "ontologies")
+
+
+ONTOLOGY_URLS = {
+    "ENSGLOSSARY": "https://raw.githubusercontent.com/Ensembl/ensembl-glossary/master/ensembl-glossary.obo",
+    "EFO": "https://www.ebi.ac.uk/efo/efo.obo",
+}
+
 
 class MyProgressBar:
     def __init__(self):
@@ -24,9 +35,13 @@ class MyProgressBar:
 def load_ontology(ontology_id: str):
     if ontology_id.isalpha():
         print(ontology_id)
-        url_alt = "https://www.ebi.ac.uk/efo/EFO.obo"
-        url = "http://purl.obolibrary.org/obo/{}.obo".format(ontology_id.lower())
-        path = "/beacon/connections/mongo/ontologies/{}.obo".format(ontology_id)
+        url_alt = "https://www.ebi.ac.uk/efo/efo.obo"
+        url = ONTOLOGY_URLS.get(
+            ontology_id.upper(),
+            "http://purl.obolibrary.org/obo/{}.obo".format(ontology_id.lower()),
+        )
+        path = os.path.join(ONTOLOGIES_DIR, "{}.obo".format(ontology_id.lower()))
+
         try:
             if not os.path.exists(path):
                 full_path = os.path.realpath(__file__)
@@ -66,81 +81,89 @@ def get_descendants_and_similarities():
     except Exception:
         db=client['beacon'].create_collection(name="similarities")
     filtering_docs=client['beacon'].filtering_terms.find({"type": "ontology"})
-    array_of_ontologies=[]
+    ontologies_by_prefix={}
     for ft_doc in filtering_docs:
-        if ft_doc["id"] not in array_of_ontologies:
-            array_of_ontologies.append(ft_doc["id"])
-    for ontology in array_of_ontologies:
-        ontology_list = ontology.split(':')
-        load_ontology(ontology_list[0])    
-        url = "/beacon/connections/mongo/ontologies/{}.obo".format(ontology_list[0].lower())
-        list_of_cousins = []
-        list_of_brothers = []
-        list_of_uncles = []
-        list_of_grandpas = []
-        url_alt = "https://www.ebi.ac.uk/efo/EFO.obo"
+        prefix = ft_doc["id"].split(':')[0]
+        if ft_doc["id"] not in ontologies_by_prefix.setdefault(prefix, []):
+            ontologies_by_prefix[prefix].append(ft_doc["id"])
+
+    for prefix, ontology_ids in ontologies_by_prefix.items():
+        load_ontology(prefix)
+        url = os.path.join(ONTOLOGIES_DIR, "{}.obo".format(prefix.lower()))
+        url_alt = "https://www.ebi.ac.uk/efo/efo.obo"
         try:
             graph = obonet.read_obo(url)
         except Exception:
-            graph = obonet.read_obo(url_alt)
-        try:
-            descendants = networkx.ancestors(graph, ontology)
-        except Exception:
-            descendants = ''
-        descendants=list(descendants)
+            try:
+                graph = obonet.read_obo(url_alt)
+            except Exception as exc:
+                print("Skipping {}: {}".format(prefix, exc))
+                continue
 
-        print(descendants)
+        for ontology in ontology_ids:
+            list_of_cousins = []
+            list_of_brothers = []
+            list_of_uncles = []
+            list_of_grandpas = []
+            try:
+                descendants = networkx.ancestors(graph, ontology)
+            except Exception:
+                descendants = ''
+            descendants=list(descendants)
+
+            print(descendants)
+
+            try:
+                tree = [n for n in graph.successors(ontology)]
+                for onto in tree:
+                    predecessors = [n for n in graph.successors(onto)]
+                    successors = [n for n in graph.predecessors(onto)]
+                    list_of_brothers.append(successors)
+                    list_of_grandpas.append(predecessors)
+                similarity_high=[]
+                similarity_medium=[]
+                similarity_low=[]
+                for llista in list_of_grandpas:
+                    for item in llista:
+                        uncles = [n for n in graph.predecessors(item)]
+                        list_of_uncles.append(uncles)
+                        for uncle in uncles:
+                            cousins = [n for n in graph.predecessors(uncle)]
+                            if ontology not in cousins:
+                                list_of_cousins.append(cousins)
+
+                for llista in list_of_brothers:
+                    for item in llista:
+                        similarity_high.append(item)
+                        similarity_medium.append(item)
+                        similarity_low.append(item)
+
+                for llista in list_of_cousins:
+                    for item in llista:
+                        similarity_medium.append(item)
+                        similarity_low.append(item)
+
+                for llista in list_of_uncles:
+                    for item in llista:
+                        similarity_low.append(item)
+
+            except Exception:
+                similarity_high=[]
+                similarity_medium=[]
+                similarity_low=[]
+
+            dict={}
+            dict['id']=ontology
+            dict['descendants']=descendants
+            dict['similarity_high']=similarity_high
+            dict['similarity_medium']=similarity_medium
+            dict['similarity_low']=similarity_low
+
+            client['beacon'].similarities.insert_one(dict)
+            print("succesfully retrieved descendants from {}".format(ontology))
+
+        del graph
+        gc.collect()
 
 
-        try:
-            tree = [n for n in graph.successors(ontology)]
-            for onto in tree:
-                predecessors = [n for n in graph.successors(onto)]
-                successors = [n for n in graph.predecessors(onto)]
-                list_of_brothers.append(successors)
-                list_of_grandpas.append(predecessors)
-            similarity_high=[]
-            similarity_medium=[]
-            similarity_low=[]
-            for llista in list_of_grandpas:
-                for item in llista:
-                    uncles = [n for n in graph.predecessors(item)]
-                    list_of_uncles.append(uncles)
-                    for uncle in uncles:
-                        cousins = [n for n in graph.predecessors(uncle)]
-                        if ontology not in cousins:
-                            list_of_cousins.append(cousins)
-
-            for llista in list_of_brothers:
-                for item in llista:
-                    similarity_high.append(item)
-                    similarity_medium.append(item)
-                    similarity_low.append(item)
-
-            for llista in list_of_cousins:
-                for item in llista:
-                    similarity_medium.append(item)
-                    similarity_low.append(item)
-            
-            for llista in list_of_uncles:
-                for item in llista:
-                    similarity_low.append(item)
-
-        except Exception:
-            similarity_high=[]
-            similarity_medium=[]
-            similarity_low=[]
-
-        dict={}
-        dict['id']=ontology
-        dict['descendants']=descendants
-        dict['similarity_high']=similarity_high
-        dict['similarity_medium']=similarity_medium
-        dict['similarity_low']=similarity_low
-        
-        client['beacon'].similarities.insert_one(dict)
-        print("succesfully retrieved descendants from {}".format(ontology))
-        
-    
 get_descendants_and_similarities()
-    
